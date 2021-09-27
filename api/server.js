ac9226364cc46c2f6344b918333b5a8c8cee0888
NeNeTheCreative <shecodes@nenethecreative.com>
2021-09-27 16:29:42 -0400

    POST is done

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const server = express()
 const User = require('./users/model')
+server.use(express.json())
 
+//[GET] api/users
 server.get('/api/users', (req, res) => {
     User.find()
     .then(users => 
@@ -12,6 +14,46 @@ server.get('/api/users', (req, res) => {
             err: err
         })
     })
+})
+
+//[GET] api/users/:id
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if (!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist" 
+            })
+        } else {
+            res.status(200).json(user)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The user information could not be retrieved",
+            err: err
+        })
+    })
+})
+
+//[POST] api/users
+server.post('/api/users', (req, res) => {
+    const user = req.body
+        if (!user.name || !user.bio) {
+            res.status(400).json(
+                {message: "Please provide name and bio for the use"})
+        } else {
+            User.insert(user)
+            .then(newUser => {
+                res.status(201).json(newUser)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error while saving the user to the database",
+                    err: err
+                })
+            })
+        }
 })
 
 
