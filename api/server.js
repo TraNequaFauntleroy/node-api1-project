7e94b56a2150d0fe276db450e91ec3bbef95b7d8
NeNeTheCreative <shecodes@nenethecreative.com>
2021-09-27 18:53:01 -0400

    added error catch to DELETE

--- api/server.js
+++ api/server.js
@@ -58,6 +58,7 @@ server.post('/api/users', (req, res) => {
 
 // [DELETE] api/users/:id
 server.delete('/api/users/:id', async (req, res) => {
+   try {
     const user = await User.findById(req.params.id)
     if (!user) {
         res.status(404).json({
@@ -67,6 +68,41 @@ server.delete('/api/users/:id', async (req, res) => {
        const deletedUser = await User.remove(user.id)
                 res.status(200).json(deletedUser)
     }
+   } catch(err) {
+    res.status(500).json({
+        message: "There was an error while saving the user to the database",
+        err: err
+   }) 
+    }
+})
+
+// [PUT] api/users/:id
+server.put('/api/users/:id', (req, res) => {
+    const { name, bio } = req.body
+    const { id } = req.params
+    if (!name || !bio ) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user" 
+        })
+    } else {
+        const updatedUser = User.update(id, {name, bio})
+        if (!updatedUser) {
+            res.status(404).json({
+               message: "The user with the specified ID does not exist"
+            })
+        } else {
+            res.json(updatedUser)
+        } 
+    if (!id) {
+        res.status(500).json({
+            message: "The user information could not be modified"
+        })
+    }  else {
+        res.status(200).json(updatedUser)
+    }  
+    }
+   
+   
 })
 
 
