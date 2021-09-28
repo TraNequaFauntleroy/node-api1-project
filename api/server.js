92836f96f7eedf2325913aac071781d42aabf363
NeNeTheCreative <shecodes@nenethecreative.com>
2021-09-28 14:21:05 -0400

    fixed my bug in PUT endpoint

--- api/server.js
+++ api/server.js
@@ -77,32 +77,32 @@ server.delete('/api/users/:id', async (req, res) => {
 })
 
 // [PUT] api/users/:id
-server.put('/api/users/:id', (req, res) => {
-    const { name, bio } = req.body
-    const { id } = req.params
-    if (!name || !bio ) {
-        res.status(400).json({
-            message: "Please provide name and bio for the user" 
+server.put('/api/users/:id', async (req, res) => {
+   try {
+    const potentialUser = await User.findById(req.params.id)
+    if (!potentialUser) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist"
         })
     } else {
-        const updatedUser = User.update(id, {name, bio})
-        if (!updatedUser) {
-            res.status(404).json({
-               message: "The user with the specified ID does not exist"
+        if (!req.body.name || !req.body.bio) {
+            res.status(400).json({
+                message: "Please provide name and bio for the user"
             })
         } else {
-            res.json(updatedUser)
-        } 
-    if (!id) {
-        res.status(500).json({
-            message: "The user information could not be modified"
-        })
-    }  else {
-        res.status(200).json(updatedUser)
-    }  
-    }
-   
-   
+           const updatedUser = await User.update(
+               req.params.id, 
+               req.body
+            )
+           res.status(200).json(updatedUser)
+        }
+      }
+   } catch (err) {
+    res.status(500).json({
+        message: "The user information could not be modified",
+        err: err
+   }) 
+  }
 })
 
 
